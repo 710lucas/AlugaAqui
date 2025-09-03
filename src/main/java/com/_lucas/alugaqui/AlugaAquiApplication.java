cb3706ae8fcce8874e5eda3744b169fedd38f538
710lucas <lucasperonicobarbotin@gmail.com>
2025-09-03 17:30:45 -0300

    initial commit

---
+++ src/main/java/com/_lucas/alugaqui/AlugaAquiApplication.java
@@ -0,0 +1,13 @@
+package com._lucas.alugaqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlugaAquiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlugaAquiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_lucas/alugaqui/AlugaAquiApplicationTests.java
@@ -0,0 +1,13 @@
+package com._lucas.alugaqui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlugaAquiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
