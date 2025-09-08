ea11e94da7307eb8eb4c7c4dfe4f2bb588de821c
710lucas <lucasperonicobarbotin@gmail.com>
2025-09-08 10:39:21 -0300

    Controller de usuario

---
+++ src/main/java/com/_lucas/alugaqui/controllers/UsuarioController.java
@@ -0,0 +1,47 @@
+package com._lucas.alugaqui.controllers;
+
+import com._lucas.alugaqui.DTOs.UsuarioCreateDTO;
+import com._lucas.alugaqui.DTOs.UsuarioUpdateDTO;
+import com._lucas.alugaqui.models.Usuario.Usuario;
+import com._lucas.alugaqui.services.UsuarioService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    public UsuarioController (UsuarioService usuarioService){
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping("/")
+    public Usuario create (@Valid @RequestBody UsuarioCreateDTO createDTO) {
+        return this.usuarioService.create(createDTO);
+    }
+
+    @GetMapping("/{id}")
+    public Usuario get (@PathVariable Long id) {
+        return this.usuarioService.get(id);
+    }
+
+    @GetMapping()
+    public Collection<Usuario> getAll (){
+        return this.usuarioService.getAll();
+    }
+
+    @PatchMapping("/{id}")
+    public Usuario update (@PathVariable Long id, @Valid @RequestBody UsuarioUpdateDTO updateDTO) {
+        return this.usuarioService.update(id, updateDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete (@PathVariable Long id) {
+        this.usuarioService.delete(id);
+    }
+
+}
